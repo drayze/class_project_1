af78da568cb1d87b342097a825d58e24133e28b7
Paul Slinkard <slinkardpaul50@gmail.com>
2026-01-30 17:04:42 -0600

    feat: Enhance gradient and text styling
    This commit refines the user interface with two main improvements:
    
    - The `LinearGradient` direction is now set to run from the top-left to the bottom-right corner for a more defined visual flow.
    - The "Hello World!" text is styled to be more prominent, with an increased font size, bold weight, and white color to improve readability against the gradient background.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Container( decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple, Colors.red, Colors.orange, Colors.yellow, ]),),
+          gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple, Colors.red, Colors.orange, Colors.yellow,],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight),),
         child: const Center(
-          child: Text('Hello World!'),
+          child: Text('Hello World!', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
         ),
       ),
     ),
