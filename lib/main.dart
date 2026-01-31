f4e2a0dd5ed41c4385b420aad0583ce8c3c30c59
Paul Slinkard <slinkardpaul50@gmail.com>
2026-01-31 07:02:11 -0600

    feat: Refactor UI into a stateless widget
    This commit refactors the main user interface by extracting the `GradientContainer` into its own `StatelessWidget`.
    
    This change improves code organization and reusability by separating the gradient background and centered text from the main application entry point. The `main` function is now simplified to run the `MaterialApp` with `GradientContainer` as its body.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp( const MaterialApp(
     home: Scaffold(
-      body: Container( decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple, Colors.red, Colors.orange, Colors.yellow,],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight),),
-        child: const Center(
-          child: Text('Hello World!', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
-        ),
-      ),
+      body: GradientContainer(),
     ),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build (context) {
+    return Container( decoration: BoxDecoration(
+      gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple, Colors.red, Colors.orange, Colors.yellow,],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight),),
+      child: const Center(
+        child: Text('Hello World!', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
+      ),
+    );
+  }
 }
