4b3c75a92ceec25d415219490b30ccab90b326a3
Paul Slinkard <slinkardpaul50@gmail.com>
2026-01-31 07:34:47 -0600

    feat: Refactor gradient alignment into constants
    This commit improves code readability and maintainability within the `GradientContainer` widget.
    
    The `begin` and `end` alignment properties of the `LinearGradient` are extracted into `start` and `end` `const` variables. This change makes the code cleaner and easier to modify in the future.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:class_project_1/custom_text.dart';
 
+const start = Alignment.topLeft;
+const end = Alignment.bottomRight;
+
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build (context) {
-    return Container( decoration: BoxDecoration(
-      gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple, Colors.red, Colors.orange, Colors.yellow,],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight),),
+    return Container( decoration: const BoxDecoration(
+      gradient: LinearGradient(colors: [Colors.green, Colors.blue, Colors.purple,
+        Colors.red, Colors.orange, Colors.yellow,],
+          begin: start,
+          end: end),),
       child: const Center(
         child: CustomText(),
       ),
